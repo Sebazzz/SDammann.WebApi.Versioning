7a7ceae94b3cf725cfa7d7bd1844f71f0037029a
Sebastiaan Dammann <triadsebas@gmail.com>
2013-03-12 19:02:53 +0100

    Implemented AcceptHeaderControllerSelector

---
+++ src/SDammann.WebApi.Versioning/AcceptHeaderControllerSelector.cs
@@ -0,0 +1,45 @@
+﻿namespace SDammann.WebApi.Versioning {
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Net.Http.Headers;
+    using System.Web.Http;
+
+    /// <summary>
+    /// Version controller that enables version by MIME type in the accept header
+    /// </summary>
+    public sealed class AcceptHeaderControllerSelector : AcceptHeaderVersionedControllerSelectorBase {
+        /// <summary>
+        /// Specifies the media type to accept. Set this in your Application_Start or before.
+        /// </summary>
+        public static string AcceptMediaType = "application/json";
+
+        /// <summary>
+        /// Initializes the <see cref="AcceptHeaderControllerSelector"/> instance
+        /// </summary>
+        /// <param name="configuration"></param>
+        public AcceptHeaderControllerSelector(HttpConfiguration configuration) : base(configuration) {}
+
+        protected override int? GetVersion(MediaTypeWithQualityHeaderValue mimeType) {
+            if (!String.Equals(mimeType.MediaType, AcceptMediaType, StringComparison.InvariantCultureIgnoreCase)) {
+                return null;
+            }
+
+            // get version
+            NameValueHeaderValue versionParameter =
+                mimeType.Parameters.FirstOrDefault(parameter => parameter.Name == "version");
+
+            if (versionParameter == null) {
+                return null;
+            }
+
+            // parse version
+            int version;
+            if (!Int32.TryParse(versionParameter.Value, NumberStyles.None, CultureInfo.InvariantCulture, out version)) {
+                return null;
+            }
+
+            return version;
+        }
+    }
+}
