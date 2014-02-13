8a8297b76683714f8ba18b0d802f235ae890bc06
Joachim Lovf <joachim.lovf@gmail.com>
2014-02-13 15:43:30 +0100

    Added null checks and made extension class internal.

--- src/SDammann.WebApi.Versioning/ApiDescriptionExtensions.cs
+++ src/SDammann.WebApi.Versioning/ApiDescriptionExtensions.cs
@@ -7,11 +7,25 @@ using System.Web.Http.Description;
 
 namespace SDammann.WebApi.Versioning
 {
-    public static class ApiDescriptionExtensions
+    internal static class ApiDescriptionExtensions
     {
         public static void SetResponseDescription(this ApiDescription apiDescription, ResponseDescription responseDescription)
         {
-            apiDescription.GetType().GetProperty("ResponseDescription").SetValue(apiDescription, responseDescription);
+            var property = GetPropertyFrom(apiDescription);
+            SetPropertyTo(apiDescription, responseDescription, property);
+        }
+
+        private static System.Reflection.PropertyInfo GetPropertyFrom(ApiDescription apiDescription)
+        {
+            if (apiDescription == null)
+                throw new ArgumentNullException("apiDescription");
+            return apiDescription.GetType().GetProperty("ResponseDescription");
+        }
+
+        private static void SetPropertyTo(ApiDescription apiDescription, ResponseDescription responseDescription, System.Reflection.PropertyInfo property)
+        {
+            if (property != null)
+                property.SetValue(apiDescription, responseDescription);
         }
     }
 }
