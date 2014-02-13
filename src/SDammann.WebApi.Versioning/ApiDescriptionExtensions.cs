93029c4e727216e3cebe84513e2e0d8598c6ab53
Joachim Lovf <joachim.lovf@gmail.com>
2014-02-13 15:30:06 +0100

    Adding reflected Set extension to ApiDescription

---
+++ src/SDammann.WebApi.Versioning/ApiDescriptionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Description;
+
+namespace SDammann.WebApi.Versioning
+{
+    public static class ApiDescriptionExtensions
+    {
+        public static void SetResponseDescription(this ApiDescription apiDescription, ResponseDescription responseDescription)
+        {
+            apiDescription.GetType().GetProperty("ResponseDescription").SetValue(apiDescription, responseDescription);
+        }
+    }
+}
