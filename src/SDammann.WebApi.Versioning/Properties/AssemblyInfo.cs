bcf684ec0a49025eb01902dab7f3d2c8154286a7
Sebazzz <triadsebas@gmail.com>
2014-09-02 23:14:51 +0200

    Bump version number

--- src/SDammann.WebApi.Versioning/Properties/AssemblyInfo.cs
+++ src/SDammann.WebApi.Versioning/Properties/AssemblyInfo.cs
@@ -1,36 +1,16 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("ASP.NET Web API Versioning module")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Damsteen.nl")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ASP.NET Web API Versioning")]
 [assembly: AssemblyCopyright("Copyright © Sebastiaan Dammann 2012 - 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bd7cdad1-3775-49f2-85ed-0a0571c74f12")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.3")]
-[assembly: AssemblyInformationalVersion("2.0.0.3")]
-[assembly: AssemblyFileVersion("2.0.0.3")]
+[assembly: AssemblyVersion("2.6.0.0")]
+[assembly: AssemblyInformationalVersion("2.6.0.0")]
+[assembly: AssemblyFileVersion("2.6.0.0")]
