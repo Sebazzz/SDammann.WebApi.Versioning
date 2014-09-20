b719bc7ddf11760f512ad6d93d22916e3a2bef0b
Sebazzz <triadsebas@gmail.com>
2014-09-20 18:35:01 +0200

    Added integration test

---
+++ src/SDammann.WebApi.Versioning.Tests/Integration/Controllers/Version1/HelloController.cs
@@ -0,0 +1,10 @@
+﻿namespace SDammann.WebApi.Versioning.Tests.Integration.Controllers.Version1 {
+    using System.Web.Http;
+
+    public sealed class HelloController : ApiController {
+
+        public string Get() {
+            return "Version1.0";
+        }
+    }
+}
