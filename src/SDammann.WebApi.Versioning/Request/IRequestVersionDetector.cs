4b22d19314c0ee142379e59f56e9306ad3049265
Sebazzz <triadsebas@gmail.com>
2014-12-04 21:18:36 +0100

    Documented allowed exception use

--- src/SDammann.WebApi.Versioning/Request/IRequestVersionDetector.cs
+++ src/SDammann.WebApi.Versioning/Request/IRequestVersionDetector.cs
@@ -6,10 +6,11 @@
     /// </summary>
     public interface IRequestVersionDetector {
         /// <summary>
-        /// Gets the <see cref="ApiVersion"/> requested from the request. Returns null if no API version could be detected.
+        /// Gets the <see cref="ApiVersion"/> requested from the request. Returns null if no API version could be detected, may throw a <see cref="ApiVersionFormatException"/> if the API version cannot be parsed
         /// </summary>
         /// <param name="requestMessage"></param>
         /// <returns></returns>
+        /// <exception cref="ApiVersionFormatException">Thrown when the API version cannot be parsed because of an invalid format</exception>
         ApiVersion GetVersion(HttpRequestMessage requestMessage);
     }
 }
