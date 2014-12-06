803adc2780f6388347e363e0aa8382e065d7b848
Sebazzz <triadsebas@gmail.com>
2014-12-06 22:49:49 +0100

    Wrote some test sample documentaiton

--- src/SDammann.WebApi.Versioning.Tests/Integration/Controllers/Version1/HelloController.cs
+++ src/SDammann.WebApi.Versioning.Tests/Integration/Controllers/Version1/HelloController.cs
@@ -5,8 +5,17 @@ namespace SDammann.WebApi.Versioning.Tests.Integration.Controllers.Version1 {
 #endif
     using System.Web.Http;
 
+    /// <summary>
+    /// Hello controller version 1 description
+    /// </summary>
+    /// <remarks>
+    /// Hello controller version 1 remarks
+    /// </remarks>
     public sealed class HelloController : ApiController {
-
+        /// <summary>
+        /// Hello controller version 1 'GET' method description
+        /// </summary>
+        /// <returns>Description of return value</returns>
         public string Get() {
             return "Version1.0";
         }
--- src/SDammann.WebApi.Versioning.Tests/Integration/Controllers/Version2_5/HelloController.cs
+++ src/SDammann.WebApi.Versioning.Tests/Integration/Controllers/Version2_5/HelloController.cs
@@ -5,7 +5,17 @@ namespace SDammann.WebApi.Versioning.Tests.Integration.Controllers.Version2_5 {
 #endif
     using System.Web.Http;
 
+    /// <summary>
+    /// Hello controller version 2.5 description
+    /// </summary>
+    /// <remarks>
+    /// Hello controller version 2.5 remarks
+    /// </remarks>
     public sealed class HelloController : ApiController {
+        /// <summary>
+        /// Hello controller version 2.5 'GET' method description
+        /// </summary>
+        /// <returns>Description of return value</returns>
         public string Get() {
             return "Version2.5";
         }
