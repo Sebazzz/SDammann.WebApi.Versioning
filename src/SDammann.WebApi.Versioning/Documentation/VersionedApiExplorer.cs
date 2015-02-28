f41c94f70d623c3ca6620b9e35d6699e2dc186f0
Sebazzz <triadsebas@gmail.com>
2015-02-28 10:55:16 +0100

    Remove usings

--- src/SDammann.WebApi.Versioning/Documentation/VersionedApiExplorer.cs
+++ src/SDammann.WebApi.Versioning/Documentation/VersionedApiExplorer.cs
@@ -1,35 +1,21 @@
 ﻿namespace SDammann.WebApi.Versioning.Documentation {
     using System;
-    using System.Collections;
-    using System.Collections.Generic;
     using System.Collections.ObjectModel;
-    using System.Diagnostics.Contracts;
-    using System.Globalization;
-    using System.Linq;
-    using System.Net.Http;
-    using System.Net.Http.Formatting;
-    using System.Reflection;
-    using System.Text.RegularExpressions;
     using System.Web.Http;
-    using System.Web.Http.Controllers;
     using System.Web.Http.Description;
-    using System.Web.Http.Dispatcher;
-    using System.Web.Http.ModelBinding.Binders;
-    using System.Web.Http.Routing;
-    using System.Web.Http.Services;
 
     /// <summary>
-    /// Represents an <see cref="IApiExplorer"/> that supports API versioning
+    ///     Represents an <see cref="IApiExplorer" /> that supports API versioning
     /// </summary>
     public class VersionedApiExplorer : IApiExplorer {
-        private readonly ApiExplorer _defaultApiExplorer;
-        private readonly HttpConfiguration _configuration;
-        private readonly Lazy<Collection<ApiDescription>> _apiDescriptions;
         private const string ActionVariableName = "action";
         private const string ControllerVariableName = "controller";
+        private readonly Lazy<Collection<ApiDescription>> _apiDescriptions;
+        private readonly HttpConfiguration _configuration;
+        private readonly ApiExplorer _defaultApiExplorer;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="T:System.Object"/> class.
+        ///     Initializes a new instance of the <see cref="T:System.Object" /> class.
         /// </summary>
         public VersionedApiExplorer(HttpConfiguration configuration) {
             this._configuration = configuration;
@@ -38,11 +24,10 @@
         }
 
         /// <summary>
-        /// Gets the API descriptions. The descriptions are initialized on the first access. 
+        ///     Gets the API descriptions. The descriptions are initialized on the first access.
         /// </summary>
         public Collection<ApiDescription> ApiDescriptions {
             get { return this._apiDescriptions.Value; }
         }
     }
-
 }
